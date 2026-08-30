be731f411f72c0b7942b77edcff679c1b08185e5
Md Alwalid <82870495+Alwalid-Akash@users.noreply.github.com>
2026-08-30 20:15:37 +0200

    Fix case sensitivity in User model import

--- backend/src/controllers/passwordController.js
+++ backend/src/controllers/passwordController.js
@@ -1,5 +1,5 @@
 const crypto = require("crypto");
-const User = require("../models/User");
+const User = require("../models/user");
 const sendEmail = require("../utils/sendEmail");
 const bcrypt = require("bcrypt");
 
